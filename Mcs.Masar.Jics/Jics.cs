f148d214c23eab1ab952cd2f27c77bfcd1b9cef6
Khalafallah55 <mohamedkhalafallah4@gmail.com>
2024-12-02 15:18:51 +0300

    add commit

--- Mcs.Masar.Jics/Jics.cs
+++ Mcs.Masar.Jics/Jics.cs
@@ -2,6 +2,6 @@
 {
     public class Jics
     {
-        public string DocmentmJicsValue { set {; } get { return "Add test jics"; } }
+        public string DocmentmJicsValue { set {; } get { return "Add test jics from Origin"; } }
     }
 }
