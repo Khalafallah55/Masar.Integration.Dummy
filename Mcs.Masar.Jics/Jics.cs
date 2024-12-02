34d31bfcb94ac0b3a3ebfe335a1d224e035086b3
Khalafallah55 <mohamedkhalafallah4@gmail.com>
2024-12-02 10:38:09 +0300

    Add Changes

--- Mcs.Masar.Jics/Jics.cs
+++ Mcs.Masar.Jics/Jics.cs
@@ -2,6 +2,6 @@
 {
     public class Jics
     {
-        public string DocmentmJicsValue { set {; } get { return "Jics2"; } }
+        public string DocmentmJicsValue { set {; } get { return "Jics22"; } }
     }
 }
